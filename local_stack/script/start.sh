be9c4aa639c7c32c23f0b7f29fc3f9649bfa0cf1
Eric Rosendale <eric.r.rosendale@gmail.com>
2019-09-02 15:35:24 -0400

    neo container name

--- local_stack/script/start.sh
+++ local_stack/script/start.sh
@@ -3,12 +3,12 @@ docker-compose up -d
 
 # Wait until a cypher-shell command returns no error on it's stderr, so we know the db is ready
 echo "Waiting for neo4j..."
-while [ -n "$(docker exec -i local_stack_neo4j_1 cypher-shell -u neo4j -p test 'MATCH (n) RETURN n' 2>&1 > /dev/null)" ]; do
+while [ -n "$(docker exec -i tag-match-neo cypher-shell -u neo4j -p test 'MATCH (n) RETURN n' 2>&1 > /dev/null)" ]; do
     printf "."
     sleep 0.1
 done
 
-cat neo4j/schema.cql | docker exec -i local_stack_neo4j_1 cypher-shell -u neo4j -p test
+cat neo4j/schema.cql | docker exec -i tag-match-neo cypher-shell -u neo4j -p test
 echo "\nNeo4j indexes created"
 
 # Not necessary for tests since supertest can startup and stop the app
