525da9653e53e4436b2c8f4997af5e6acef6fdc9
Eric Rosendale <eric.r.rosendale@gmail.com>
2019-09-11 20:36:05 -0400

    use the auth db

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 const dbUrl = process.env.MONGO_URL || "127.0.0.1";
-const dbCollection = process.env.MONGO_AUTH_DB || "auth-test";
+const dbCollection = process.env.MONGO_AUTH_DB || "auth";
 const dbUsername = process.env.MONGO_AUTH_USERNAME || "root";
 const dbPassword = process.env.MONGO_AUTH_PASSWORD || "password";
 //sets the required variables from Environment Variables.
