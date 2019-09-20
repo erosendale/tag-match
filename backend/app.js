07a7a3d78fff14752e86e537144c0b0337b7b4dc
Eric Rosendale <eric.r.rosendale@gmail.com>
2019-09-20 19:14:12 -0400

    allow local cors host

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,12 @@ const app = express();
 // Enable cors
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "127.0.0.1"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, api_key, Authorization");
+  next();
+});
+
 // Establish a connection to mongodb
 mongo.getConnection();
   
